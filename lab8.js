5018e3b5e15f33fd289cf2ba3fc4485e22962b67
Sofia <sbabinova0678@gmail.com>
2023-06-05 19:58:05 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -37,3 +37,9 @@ function Del() {
     document.getElementById('KBD').innerHTML = '';
 }
 
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+}
+setInterval(showTime, 1000)
